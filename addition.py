e0648b0d976641ae099515f780b949e2d569760e
Venkata Phanendra Sainadh Narne <venkata.n@prudentconsulting.com>
2024-10-29 10:13:37 -0500

    python file has been added in the addition of programming

---
+++ addition.py
@@ -0,0 +1,8 @@
+python
+Copy code
+import numpy as np
+
+matrix1 = np.array([[1, 2], [3, 4]])
+matrix2 = np.array([[5, 6], [7, 8]])
+result = matrix1 + matrix2
+print("Matrix addition result:\n", result)
